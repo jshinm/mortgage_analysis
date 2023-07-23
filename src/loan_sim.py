e5e21ee7d8d0a7c41c3acfbf8f63c4bd04350754
Jong Shin <jshin.m@gmail.com>
2023-07-23 17:07:45 -0400

    convert codebase to oop

--- src/loan_sim.py
+++ src/loan_sim.py
@@ -1,83 +1,128 @@
 # Amortized Loan Simulator
+import warnings
 import pandas as pd
+import matplotlib.pyplot as plt
 
-pd.set_option('display.max_rows', None)
+warnings.filterwarnings("ignore")
 
-def calculate_interest(P,r):
-    '''Calculate simple monthly interest payment
-    '''
-    
-    return P*r/12
-    
-def calculate_snapshot_monthly(P, r, n):
-    '''Calculate amortized monthly payment
-    
-    P (int): initial principal
-    r (float): interest per period (e.g., APR/12)
-    n (int): total number of payments
-    '''
-    r /= 12 #interest per month
-    A = P / ( (1+r)**n-1 ) * (r*(1+r)**n)
-    
-    return A
 
-def find_optimal_schedule(P,r,n,test_range,M=None):
-    '''Creates table to find the optimal overpayment schedule
-    '''
-    cols = ['Additive Down', 'Total Payment Schedule', 'Total Interest Paid']
-    df = pd.DataFrame(columns=cols)
-    
-    for rng in test_range:
-        total_schedule, total_interest = generate_amortization_table(P=P, r=r, n=n, M=M, additive=rng, return_range=True)
-        df = pd.concat([df, pd.DataFrame([rng, total_schedule, total_interest], index=cols).T])
-    
-    df.reset_index(drop=True, inplace=True)
-    return df
+class LoanCalc:
+    def __init__(self, P, r, n, M=None, additive=0, return_range=False, max_row=None):
+        self.P = P
+        self.r = r
+        self.n = n
+        self.M = M
+        self.additive = additive
 
-def generate_amortization_table(P,r,n,M=None,additive=0,return_range=False):
-    '''Generate complete amortization schedule
-    
-    P (int): initial principal
-    r (float): interest per period (e.g., APR/12)
-    n (int): total number of payments
-    M (float, optional): total payment amount
-    additive (float, optional): additional monthly principal payment
-    '''
-    cols = ['Beginning Balance', 'Total Pmt', 'Interest', 
-            'Principal', 'Ending Balance', 'Total Principal', 
-            'Total Interest', 'Percent Paid']
-    df = pd.DataFrame(columns=cols)
-    if not M:
-        M = calculate_snapshot_monthly(P=P, r=r, n=n)+additive #monthly total (this is constant)
-    TI = 0 #total interest
-    PI = 0 #total principal
-    P_initial = P #initial principal balance
-    
-    for i in range(n):
-        I = calculate_interest(P=P, r=r)
-        P_i = P #initial balance
-        pr = M-I #principal
-        P -= pr #ending balance
-        TI += I #total interest
-        PI += pr #total principal
-        perc_paid = (PI / P_initial)*100 #percent principal paid
+        pd.set_option("display.max_rows", max_row)
 
-        if P < 0: 
-            M = P_i
-            pr = M
-            P = 0
+    @staticmethod
+    def _calculate_interest(P, r):
+        """Calculate simple monthly interest payment"""
+        return P * r / 12
 
-        tmp = [P_i, M, I, pr, P, PI, TI, perc_paid]
-        tmp = [round(j,2) for j in tmp]
-        
-        df = pd.concat([df, pd.DataFrame(tmp, index=cols).T])
+    @staticmethod
+    def _calculate_snapshot_monthly(P, r, n):
+        """Calculate amortized monthly payment
 
-        if P == 0: #break cycle if ends early
-            break
+        P (int): initial principal
+        r (float): interest per period (e.g., APR/12)
+        n (int): total number of payments
+        """
+        # P = self.P
+        # r = self.r
+        # n = self.n
 
-    df = df.reset_index(drop=True)
-    df.index += 1
-    if return_range:
-        return max(df.index), TI
-    else:
+        r /= 12  # interest per month
+        A = P / ((1 + r) ** n - 1) * (r * (1 + r) ** n)
+
+        return A
+
+    def find_optimal_schedule(self, search_range=None, M=None, draw_plot=True):
+        """Creates table to find the optimal overpayment schedule"""
+
+        cols = ["Additive Down", "Total Payment Schedule", "Total Interest Paid"]
+        df = pd.DataFrame(columns=cols)
+
+        if search_range is None:
+            maxval = int(self.P / self.n)
+            step = int(maxval * 0.1)
+            search_range = range(0, maxval, step)
+
+        for rng in search_range:
+            total_schedule, total_interest = self.generate_amortization_table(
+                P=self.P, r=self.r, n=self.n, M=self.M, additive=rng, return_range=True
+            )
+
+            df = pd.concat(
+                [df, pd.DataFrame([rng, total_schedule, total_interest], index=cols).T]
+            )
+
+        df.reset_index(drop=True, inplace=True)
+
+        # plot schedule
+        if draw_plot:
+            fig, ax = plt.subplots(1, 1, figsize=(6 * 3, 6))
+            df.plot(x="Additive Down", y="Total Payment Schedule", kind="bar", ax=ax)
+
         return df
+
+    def generate_amortization_table(
+        self, P, r, n, M=None, additive=0, return_range=False
+    ):
+        """Generate complete amortization schedule
+
+        P (int): initial principal
+        r (float): interest per period (e.g., APR/12)
+        n (int): total number of payments
+        M (float, optional): total payment amount
+        additive (float, optional): additional monthly principal payment
+        """
+        cols = [
+            "Beginning Balance",
+            "Total Pmt",
+            "Interest",
+            "Principal",
+            "Ending Balance",
+            "Total Principal",
+            "Total Interest",
+            "Percent Paid",
+        ]
+
+        df = pd.DataFrame(columns=cols)
+        if not M:
+            M = (
+                self._calculate_snapshot_monthly(P=P, r=r, n=n) + additive
+            )  # monthly total (this is constant)
+        TI = 0  # total interest
+        PI = 0  # total principal
+        P_initial = P  # initial principal balance
+
+        for i in range(n):
+            I = self._calculate_interest(P=P, r=r)
+            P_i = P  # initial balance
+            pr = M - I  # principal
+            P -= pr  # ending balance
+            TI += I  # total interest
+            PI += pr  # total principal
+            perc_paid = (PI / P_initial) * 100  # percent principal paid
+
+            if P < 0:
+                M = P_i
+                pr = M
+                P = 0
+
+            tmp = [P_i, M, I, pr, P, PI, TI, perc_paid]
+            tmp = [round(j, 2) for j in tmp]
+
+            df = pd.concat([df, pd.DataFrame(tmp, index=cols).T])
+
+            if P == 0:  # break cycle if ends early
+                break
+
+        df = df.reset_index(drop=True)
+        df.index += 1
+        if return_range:
+            return max(df.index), TI
+        else:
+            return df
